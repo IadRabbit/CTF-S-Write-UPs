0d83ae53506d5add218e96fd416770a1b14181b4
Federico Zota <87022719+IadRabbit@users.noreply.github.com>
2021-09-22 23:19:41 +0200

    Update arp_ping.py

--- userful_scripts/arp_ping.py
+++ userful_scripts/arp_ping.py
@@ -6,18 +6,18 @@ from scapy.layers.l2 import Ether, ARP, srp
 conf = Conf()
 
 def get_vendor(mac):
-	vendor = conf.manufdb._get_manuf(mac)
+	vendor = conf.manufdb._get_manuf(mac)# get mac vendor name
 
 	return vendor
 
 ans, unans = (
 	srp(
-		Ether(dst = "ff:ff:ff:ff:ff:ff")/
-		ARP(pdst = "192.168.1.0/24"),
+		Ether(dst = "ff:ff:ff:ff:ff:ff")/ # broadcast destination
+		ARP(pdst = "192.168.1.0/24"), # destination to all network
 		timeout = 2,
-		verbose = 0
+		verbose = 0 # don't show output scan
 	)
-)
+)# sending/receiving broadcast message asking for ARP addresses
 
 for answered in ans:
 	ip = answered[1].psrc
